8003ecedbc215d1450d0b6acd11f6954a16cae27
sankar kp <kpsankar6@gmail.com>
2023-04-28 09:28:48 +0530

    Ajax implemented

--- demo/home/views.py
+++ demo/home/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from django.http import JsonResponse
+from datetime import datetime
 # Create your views here.
+def homepage(req):
+    return render(req, 'index.html')
+
+def current_time(req):
+    n = datetime.now()
+    return JsonResponse({'time': str(n)})
